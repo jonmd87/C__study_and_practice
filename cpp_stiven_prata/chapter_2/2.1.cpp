f34e31e308b987daec1b6950c1012512a3b4f194
jonmd87 <jonmd87@yandex.ru>
2021-04-08 17:52:06 +0300

    chapter_2 ex 1

--- cpp_stiven_prata/chapter_2/2.1.cpp
+++ cpp_stiven_prata/chapter_2/2.1.cpp
@@ -6,15 +6,26 @@
 /*   By: evgen <jonmd87@live.com>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/07 22:04:04 by evgen             #+#    #+#             */
-/*   Updated: 2021/04/07 22:06:50 by evgen            ###   ########.fr       */
+/*   Updated: 2021/04/08 by EvGeN                     ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 using namespace std;
 
+# define INCH 2.54
+# define FOOT (INCH * 12)
+
 int				main(void)
 {
-	
+	double		height;
+
+	height = 0;
+	cout << "Please enter your height, in cm\n";
+	cin >> height;
+	cout << "Your height:\n";
+	cout << "\t_cm_[" << height << "]\n";
+	cout << "\tinch[" << height / INCH << "]\n";
+	cout << "\tfoot[" << (int)(height / FOOT) << "]\n";
 	return (0);
 }
